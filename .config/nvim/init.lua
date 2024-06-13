e573f46d7013b18e80b1a72d387e6dd48e9f429a
Michels, AJ <aj.michels@biworldwide.com>
2024-06-13 09:31:04 -0500

    nvim do not wrap lines

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -98,6 +98,9 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- do not wrap long lines
+vim.o.wrap = false
+
 -- Make line numbers default
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
