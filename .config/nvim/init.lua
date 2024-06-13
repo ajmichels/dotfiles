ab97acd0310ee89fd03da74b8ee861310f572fdc
AJ Michels <ajmichels.web@gmail.com>
2024-06-13 00:22:48 -0500

    disable autosuggest in comments
    I found it really annoying that the autosuggest was popping up when I
    was writing comments since most of what is in the comments is arbitrary
    text that probably doesn't exist in other parts of the file/code.
    
    The only downside to this is that I don't get autosuggestions when
    writing docblocks

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -783,6 +783,16 @@ require('lazy').setup({
           { name = 'luasnip' },
           { name = 'path' },
         },
+        enabled = function()
+          -- disable completion in comments
+          local context = require 'cmp.config.context'
+          -- keep command mode completion enabled when cursor is in a comment
+          if vim.api.nvim_get_mode().mode == 'c' then
+            return true
+          else
+            return not context.in_treesitter_capture 'comment' and not context.in_syntax_group 'Comment'
+          end
+        end,
       }
     end,
   },
