dec788972edd954a92f4204b714e31f30ae41484
AJ Michels <ajmichels.web@gmail.com>
2024-06-13 00:24:23 -0500

    override nvim gruvbox colors
    I found it a little distracting that there were this lighter colored bars
    on the top and side of the window, now they match the normal background

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -801,7 +801,12 @@ require('lazy').setup({
     'ellisonleao/gruvbox.nvim',
     priority = 1000,
     config = true,
-    opts = {},
+    opts = {
+      overrides = {
+        SignColumn = { bg = '#282828' },
+        TabLineFill = { bg = '#282828', fg = '#a89984' },
+      },
+    },
     init = function()
       vim.cmd.colorscheme 'gruvbox'
     end,
