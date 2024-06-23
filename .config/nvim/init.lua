7b0833f3fcb654ab1f07925ef0aaf0b8971aa3c2
AJ Michels <ajmichels.web@gmail.com>
2024-06-22 23:55:56 -0500

    nvim map ; to : to avoid needing to shift

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -91,7 +91,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- [[ Basic Autocommands ]]
+vim.keymap.set('n', ';', ':', { desc = 'Use ; for :', remap = false })
+
+-- [[ Basic Autocommands ]e
 --  See `:help lua-guide-autocommands`
 
 -- Highlight when yanking (copying) text
