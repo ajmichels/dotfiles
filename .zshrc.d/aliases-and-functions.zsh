3975b8b1db80b29177d06a1e5505f373c4ddd1f2
Michels, AJ <aj.michels@biworldwide.com>
2024-07-29 19:12:53 -0500

    add alias for watching trees with color enabled

--- .zshrc.d/aliases-and-functions.zsh
+++ .zshrc.d/aliases-and-functions.zsh
@@ -13,6 +13,7 @@ alias vim='nvim'
 alias decolorize='sed "s/\x1B\[[0-9;]\{1,\}[A-Za-z]//g"'
 alias mdfind='mdfind -onlyin "$PWD"'
 alias tree='eza -l --tree --git --icons -I node_modules\|coverage\|vendor\|build\|dist'
+alias watch-tree='watch -c "eza -l --git --tree --icons --color=always $*"'
 
 # print terminal colors
 alias show-colors='for x in {0..8}; do for i in {30..37}; do for a in {40..47}; do echo -ne "\e[$x;$i;$a""m\\\e[$x;$i;$a""m\e[0;37;40m "; done; echo; done; done; echo ""'
