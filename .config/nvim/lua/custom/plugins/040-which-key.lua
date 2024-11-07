00141fb4eef8bb4c9b393aece09573b56abe319b
AJ Michels <ajmichels.web@gmail.com>
2024-11-06 22:08:10 -0600

    update which-key

--- .config/nvim/lua/custom/plugins/040-which-key.lua
+++ .config/nvim/lua/custom/plugins/040-which-key.lua
@@ -12,28 +12,27 @@
 -- Then, because we use the `config` key, the configuration only runs
 -- after the plugin has been loaded:
 --  config = function() ... end
+--
+-- https://github.com/folke/which-key.nvim
 
 return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
-  version = 'v2.1.0',
-  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-  config = function() -- This is the function that runs, AFTER loading
-    require('which-key').setup()
-
-    -- Document existing key chains
-    require('which-key').register {
-      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-      ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-      ['<leader>o'] = { name = '[O]bsidian', _ = 'which_key_ignore' },
-    }
-    -- visual mode
-    require('which-key').register({
-      ['<leader>h'] = { 'Git [H]unk' },
-    }, { mode = 'v' })
-  end,
+  version = 'v3.13.3',
+  event = 'VimEnter',
+  opts = {
+    delay = 500,
+    win = {
+      no_overlap = false,
+    },
+    spec = {
+      { '<leader>c', group = '[C]ode' },
+      { '<leader>d', group = '[D]ocument' },
+      { '<leader>r', group = '[R]ename' },
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>w', group = '[W]orkspace' },
+      { '<leader>t', group = '[T]oggle' },
+      { '<leader>o', group = '[O]bsidian' },
+      { '<leader>g', group = '[G]it' },
+    },
+  },
 }
