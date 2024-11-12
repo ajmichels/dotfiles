beea839689fb08ac1f1f0d2275fb54886c2cd51b
Michels, AJ <aj.michels@biworldwide.com>
2024-11-12 11:45:39 -0600

    add plugin to trim whitespace

---
+++ .config/nvim/lua/custom/plugins/525-trim.lua
@@ -0,0 +1,9 @@
+-- Highlight and trim trailing whitespace in buffers
+--
+-- See https://github.com/cappyzawa/trim.nvim
+return {
+  'cappyzawa/trim.nvim',
+  opts = {
+    highlight = true,
+  },
+}
