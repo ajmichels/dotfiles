93a058fef8656533df375a636d218af0f1a892c4
AJ Michels <ajmichels.web@gmail.com>
2026-03-15 14:11:35 -0500

    update lsp configuration
    Was running into issues with the latest version of ESLint. Turns out the
    fix was in the latest version of nvim-lspconfig
    
    https://github.com/neovim/nvim-lspconfig/issues/4318

--- .config/nvim/lua/custom/plugins/030-nvim-lspconfig.lua
+++ .config/nvim/lua/custom/plugins/030-nvim-lspconfig.lua
@@ -1,37 +1,31 @@
 return { -- LSP Configuration & Plugins
   'neovim/nvim-lspconfig',
-  version = 'v2.0.0',
+  version = 'v2.7.0',
   dependencies = {
-
     -- Automatically install LSPs and related tools to stdpath for Neovim
     -- NOTE: Must be loaded before dependants
     {
       'williamboman/mason.nvim',
-      version = 'v1.11.0',
-      config = true,
+      ---@module 'mason.settings'
+      ---@type MasonSettings
+      ---@diagnostic disable-next-line: missing-fields
+      --version = 'v2.2.1',
+      opts = {},
     },
     {
-      'williamboman/mason-lspconfig.nvim',
-      version = 'v1.32.0',
+      'mason-org/mason-lspconfig.nvim',
+      version = 'v2.1.0',
     },
     {
       'WhoIsSethDaniel/mason-tool-installer.nvim',
-      commit = '1255518',
+      --commit = '1255518',
     },
 
     -- Useful status updates for LSP.
     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
     {
       'j-hui/fidget.nvim',
-      version = 'v1.6.1',
-      opts = {},
-    },
-
-    -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
-    -- used for completion, annotations and signatures of Neovim apis
-    {
-      'folke/neodev.nvim',
-      version = 'v3.0.0', -- laster version, project deprecated
+      --version = 'v1.6.1',
       opts = {},
     },
   },
@@ -73,8 +67,9 @@ return { -- LSP Configuration & Plugins
         --
         -- In this case, we create a function that lets us more easily define mappings specific
         -- for LSP related items. It sets the mode, buffer and description for us each time.
-        local map = function(keys, func, desc)
-          vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+        local map = function(keys, func, desc, mode)
+          mode = mode or 'n'
+          vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
 
         -- Jump to the definition of the word under your cursor.
@@ -108,7 +103,7 @@ return { -- LSP Configuration & Plugins
 
         -- Execute a code action, usually your cursor needs to be on top of an error
         -- or a suggestion from your LSP for this to activate.
-        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
 
         -- Opens a popup that displays documentation about the word under your cursor
         --  See `:help K` for why this keymap.
@@ -124,7 +119,7 @@ return { -- LSP Configuration & Plugins
         --
         -- When you move your cursor, the highlights will be cleared (the second autocommand).
         local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client.server_capabilities.documentHighlightProvider then
+        if client and client:supports_method('textDocument/documentHighlight', event.buf) then
           local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
           vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
             buffer = event.buf,
@@ -147,24 +142,17 @@ return { -- LSP Configuration & Plugins
           })
         end
 
-        -- The following autocommand is used to enable inlay hints in your
+        -- The following code creates a keymap to toggle inlay hints in your
         -- code, if the language server you are using supports them
         --
         -- This may be unwanted, since they displace some of your code
-        if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+        if client and client:supports_method('textDocument/inlayHint', event.buf) then
           map('<leader>th', function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
           end, '[T]oggle Inlay [H]ints')
         end
       end,
     })
-
-    -- LSP servers and clients are able to communicate to each other what features they support.
-    --  By default, Neovim doesn't support everything that is in the LSP specification.
-    --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-    --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
     -- Enable the following language servers
     --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -177,25 +165,35 @@ return { -- LSP Configuration & Plugins
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
       -- clangd = {},
-      -- gopls = {},
-      -- pyright = {},
-      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-
-      rust_analyzer = {},
 
       -- Lua Language Server configuration
       lua_ls = {
-        -- cmd = {...},
-        -- filetypes = { ...},
-        -- capabilities = {},
-        settings = {
-          Lua = {
-            completion = {
-              callSnippet = 'Replace',
+        on_init = function(client)
+          if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then
+              return
+            end
+          end
+
+          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+              version = 'LuaJIT',
+              path = { 'lua/?.lua', 'lua/?/init.lua' },
             },
-            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            -- diagnostics = { disable = { 'missing-fields' } },
-          },
+            workspace = {
+              checkThirdParty = false,
+              -- NOTE: this is a lot slower and will cause issues when working on your own configuration.
+              --  See https://github.com/neovim/nvim-lspconfig/issues/3189
+              library = vim.tbl_extend('force', vim.api.nvim_get_runtime_file('', true), {
+                '${3rd}/luv/library',
+                '${3rd}/busted/library',
+              }),
+            },
+          })
+        end,
+        settings = {
+          Lua = {},
         },
       },
 
@@ -215,12 +213,19 @@ return { -- LSP Configuration & Plugins
 
       eslint = {
         settings = {
-          debug = true,
-          nodePath = vim.env.PNPM_HOME .. '/global/5/node_modules',
+          -- Use pnpm as the package manager if a pnpm-lock.yaml file exists
+          packageManager = 'pnpm',
+          autofixOnSave = true,
+
+          -- I have been running into an issue with ESLint v10. The following issue documents this
+          -- problem: https://github.com/neovim/nvim-lspconfig/issues/4318
         },
-        flags = { debounce_text_changes = 500 },
+        flags = {
+          --debounce_text_changes = 500,
+        },
         on_attach = function(client)
           local au_lsp = vim.api.nvim_create_augroup('eslint_lsp', { clear = true })
+
           vim.api.nvim_create_autocmd('BufWritePre', {
             pattern = '*',
             callback = function()
@@ -231,44 +236,30 @@ return { -- LSP Configuration & Plugins
         end,
       },
 
-      gopls = {}, -- Go lang files
+      pyright = {}, -- Python LSP
+      rust_analyzer = {}, -- Rust LSP
+      stylua = {}, -- Lua style linter
+      gopls = {}, -- Go lang LSP
       commitlint = {}, -- Conventional Commits - Git Commit Messages
       markdownlint = {}, -- Markdown files
-      jsonlint = {},
-      vacuum = {},
+      jsonlint = {}, -- JSON Linter LSP
+      vacuum = {}, -- OpenAPI/Swagger LSP
       curlylint = {}, -- Curl commands
       gitlab_ci_ls = {}, -- GitLab CI Files
       dockerls = {}, -- Dockerfiles
       docker_compose_language_service = {}, -- Docker Compose files
     }
 
-    -- Ensure the servers and tools above are installed
-    --  To check the current status of installed tools and/or manually install
-    --  other tools, you can run
-    --    :Mason
-    --
-    --  You can press `g?` for help in this menu.
-    require('mason').setup()
-
     -- You can add other tools here that you want Mason to install
     -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      'stylua', -- Used to format Lua code
-    })
+    vim.list_extend(ensure_installed, {})
+
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
-    require('mason-lspconfig').setup {
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          -- This handles overriding only values explicitly passed
-          -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for tsserver)
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          require('lspconfig')[server_name].setup(server)
-        end,
-      },
-    }
+    for name, server in pairs(servers) do
+      vim.lsp.config(name, server)
+      vim.lsp.enable(name)
+    end
   end,
 }
